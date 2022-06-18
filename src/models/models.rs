c723582396053eb94c50a1d632518088202a70a3
Michael Valencia <emvalenc@go.olemiss.edu>
2022-06-18 00:15:30 -0500

    added tournament selection, need to do crossover, refactored tests

--- src/models/models.rs
+++ src/models/models.rs
@@ -1,4 +1,5 @@
 use crate::utils::domain::random_float;
+use rand::{Rng};
 
 #[allow(dead_code)] // The DNA Size will always be positive (non-negative) and therefore is u32.
 const DNA_SIZE: u32 = 3;
@@ -7,12 +8,13 @@ const DOMAIN_LOWER: f64 = 0.0;
 #[allow(dead_code)] 
 const DOMAIN_UPPER: f64 = 10.0;
 
-const TargetList: &'static [f64] = &[2.0, 4.0, 6.0, 8.0, 10.0];
+const TARGET_LIST: &'static [f64] = &[2.0, 4.0, 6.0, 8.0, 10.0];
+const TOURNAMENT_SIZE: u32 = 3;
 
-pub enum Organism {
-        ForceOrganism(ForceOrganism),
-        SimpleOrganism(SimpleOrganism),
-}
+// pub enum Organism {
+//         ForceOrganism(ForceOrganism),
+//         SimpleOrganism(SimpleOrganism),
+// }
 
 #[derive(Debug)]
 pub struct ForceOrganism {
@@ -21,7 +23,7 @@ pub struct ForceOrganism {
         pub fitness: f32
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct SimpleOrganism {
         pub id: i32,
         pub dna: Vec<f64>,
@@ -44,7 +46,7 @@ impl SimpleOrganism {
                 return o
         }
 
-        pub fn new_DEBUG(id: i32, dna: Vec<f64>) -> SimpleOrganism {
+        pub fn new_debug(id: i32, dna: Vec<f64>) -> SimpleOrganism {
                 let mut o = SimpleOrganism {
                         id: id,
                         dna: Vec::new(),
@@ -67,6 +69,7 @@ impl SimpleOrganism {
 
 }
 
+
 // Make the pool of organisms. 
 // TODO: Later, this should use the enum to make the pool of organisms.
 pub fn create_organism_pool(size: i32) -> Vec<SimpleOrganism> {
@@ -75,20 +78,85 @@ pub fn create_organism_pool(size: i32) -> Vec<SimpleOrganism> {
         // Create the organisms up to the size. 
         for i in 0..size {
                 let mut o = SimpleOrganism::new(i, DNA_SIZE.try_into().unwrap());
-                o.evalute_fitness(TargetList.to_vec());
+                o.evalute_fitness(TARGET_LIST.to_vec());
                 pool.push(o);
         }
         pool
 }
+
 
 fn difference_squared(a: f64, b: f64) -> f64 {
         let diff = a - b;
         diff * diff
 }
 
+pub fn eliminate_unfit_fractions(pool: &mut Vec<SimpleOrganism>) {
+        // Ensure the pool is an even number.
+        assert_eq!(pool.len() % 2, 0);
+        
+        // Sort the pool by fitness.__rust_force_expr!
+        pool.sort_by(|a, b| a.fitness.partial_cmp(&b.fitness).unwrap());
+        // Remove the top half of the pool (i.e., the least fit).
+        // TODO: Make this eliminate different fractions of the pool.
+        pool.drain((pool.len() / 2)..pool.len());
+}
+
+pub fn natural_selection(pool: &mut Vec<SimpleOrganism>) {
+        // Copy the original pool so we don't use the new organisms in the mating.
+        let new_pool = pool.clone();
+        
+        // The number of iterations should be equal to the pool since we cut it in half.
+        // That is, we must refill the pool.
+        // TODO: Refill different fractions of the pool depending on the above function.
+        for _ in 0..pool.len() {
+                let mut parents: Vec<SimpleOrganism> = Vec::new();
+                for _ in 0..3 {
+                        parents.push(tournament_round(&pool.to_vec()));
+                }
+
+                // new_pool.push(Mating());
+        }
+}
+
+pub fn tournament_round(pool: &Vec<SimpleOrganism>) -> SimpleOrganism {
+        // The number of iterations should be equal to the pool since we cut it in half.
+        // That is, we must refill the pool.
+        let mut group: Vec<SimpleOrganism> = Vec::new();
+        // Each group will have TOURNAMENT_SIZE members.
+        for _ in 0..TOURNAMENT_SIZE {
+                let mut rng = rand::thread_rng();
+                let index = rng.gen_range(0..pool.len());
+                // Is clone necessary? 
+                // Maybe there is a better way to do this with some sort of reference?
+                group.push(pool[index].clone());
+        }
+        
+        // Sort the group by fitness.
+        group.sort_by(|a, b| a.fitness.partial_cmp(&b.fitness).unwrap());
+        
+        // Return the most fit organism.
+        group[0].clone()
+}
+
 #[cfg(test)]
 mod tests {
         use super::*;
+        fn generate_test_organism() -> Vec<SimpleOrganism> {
+                let mut o1 = SimpleOrganism::new(0, 3);
+                o1.fitness = 1.0;
+                
+                let mut o2 = SimpleOrganism::new(1, 3);
+                o2.fitness = 3.0;
+
+                let mut o3 = SimpleOrganism::new(2, 3); 
+                o3.fitness = 2.0;
+
+                let mut o4 = SimpleOrganism::new(3, 3); 
+                o4.fitness = 5.0;
+
+                vec![o1, o2, o3, o4]
+        }
+
         #[test]
         fn test_evalute_fitness() {
                 let mut test_org = SimpleOrganism{id: 0, dna: vec![1.0, 1.0, 1.0], fitness: 0.0};
@@ -114,6 +182,26 @@ mod tests {
                 }
 
                 println!("{:?}", o);
+        }
+        #[test]
+        fn test_elimination() {
+                let mut pool: Vec<SimpleOrganism> = generate_test_organism();
+
+                eliminate_unfit_fractions(&mut pool);
+
+                assert_eq!(pool[0].fitness, 1.0);
+                assert_eq!(pool[1].fitness, 2.0);
+        }
+
+        #[test]
+        fn test_tournament_round() {
+                let pool: Vec<SimpleOrganism> = generate_test_organism()[1..4].to_vec(); 
+
+                let expected = pool[1].clone();
+
+                let best = tournament_round(&pool);
+
+                assert_eq!(best, expected);
         }
 
 }
--- src/traditional/traditional.rs
+++ src/traditional/traditional.rs
@@ -1,4 +1,4 @@
-use crate::models::models::{ForceOrganism, SimpleOrganism, Organism, create_organism_pool};
+use crate::models::models::{ForceOrganism, SimpleOrganism, create_organism_pool};
 
 pub fn run_tga() {
         let pool: Vec<SimpleOrganism> = create_organism_pool(100);
@@ -7,38 +7,3 @@ pub fn run_tga() {
 }
 
 // This should sort by fitness for either the force organism or the simple organism from the Organism enum.
-pub fn eliminate_unfit_fractions(pool: &mut Vec<SimpleOrganism>) {
-        // Ensure the pool is an even number.
-        assert_eq!(pool.len() % 2, 0);
-        
-        // Sort the pool by fitness.__rust_force_expr!
-        pool.sort_by(|a, b| a.fitness.partial_cmp(&b.fitness).unwrap());
-        // Remove the top half of the pool (i.e., the least fit).
-        pool.drain((pool.len() / 2)..pool.len());
-}
-
-#[cfg(test)]
-mod tests {
-        use super::*;
-        #[test]
-        fn test_elimination() {
-                let mut o1 = SimpleOrganism::new(0, 3);
-                o1.fitness = 1.0;
-                
-                let mut o2 = SimpleOrganism::new(1, 3);
-                o2.fitness = 3.0;
-
-                let mut o3 = SimpleOrganism::new(2, 3); 
-                o3.fitness = 2.0;
-
-                let mut o4 = SimpleOrganism::new(3, 3); 
-                o4.fitness = 5.0;
-
-                let mut pool: Vec<SimpleOrganism> = vec![o1, o2, o3, o4];
-
-                eliminate_unfit_fractions(&mut pool);
-
-                assert_eq!(pool[0].fitness, 1.0);
-                assert_eq!(pool[1].fitness, 2.0);
-        }
-}
