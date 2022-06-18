a87f2d8a8e324b942fdecf2844bb180047de8285
Michael Valencia <emvalenc@go.olemiss.edu>
2022-06-17 19:25:31 -0500

    playing with enums and impl of organism trait, added some tests

--- src/models/models.rs
+++ src/models/models.rs
@@ -3,60 +3,85 @@ use crate::utils::domain::random_float;
 #[allow(dead_code)] // The DNA Size will always be positive (non-negative) and therefore is u32.
 const DNA_SIZE: u32 = 3;
 #[allow(dead_code)] // The lower domain will always be 0.0 
-const DOMAIN_LOWER: f32 = 0.0;
+const DOMAIN_LOWER: f64 = 0.0;
 #[allow(dead_code)] 
-const DOMAIN_UPPER: f32 = 10.0;
+const DOMAIN_UPPER: f64 = 10.0;
+
+const TargetList: &'static [f64] = &[2.0, 4.0, 6.0, 8.0, 10.0];
+
+pub enum Organism {
+        ForceOrganism(ForceOrganism),
+        SimpleOrganism(SimpleOrganism),
+}
 
 #[derive(Debug)]
-pub struct Organism {
+pub struct ForceOrganism {
         pub id: i32,
-        pub dna: Vec<f32>,
+        pub dna: Vec<Vec<Vec<f64>>>,
         pub fitness: f32
 }
 
-impl Organism {
-        pub fn new(id: i32, dna_size: i32) -> Organism {
-                let mut o = Organism {
+#[derive(Debug)]
+pub struct SimpleOrganism {
+        pub id: i32,
+        pub dna: Vec<f64>,
+        pub fitness: f64
+}
+
+impl SimpleOrganism {
+        pub fn new(id: i32, dna_size: i32) -> SimpleOrganism {
+                let mut o = SimpleOrganism {
                         id: id,
                         dna: Vec::new(),
                         fitness: 0.0
                 };
         
                 for _ in 0..dna_size {
-                        let gene = random_float(DOMAIN_LOWER, DOMAIN_UPPER);
+                        let gene = random_float(DOMAIN_LOWER, DOMAIN_UPPER).try_into().unwrap();
                         o.dna.push(gene);
                 }
         
                 return o
         }
+
+        pub fn new_DEBUG(id: i32, dna: Vec<f64>) -> SimpleOrganism {
+                let mut o = SimpleOrganism {
+                        id: id,
+                        dna: Vec::new(),
+                        fitness: 0.0
+                };
+
+                o.dna = dna;
+                return o
+        }
+
+        pub fn evalute_fitness(&mut self, target: Vec<f64>) {
+                assert_eq!(self.dna.len(), target.len());
+                let mut sum: f64 = 0.0;
+                for i in 0..self.dna.len() {
+                        sum += difference_squared(self.dna[i], target[i]);
+                }
+
+                self.fitness = sum;
+        }
+
 }
 
-
-pub fn create_organism_pool(size: i32) -> Vec<Organism> {
-        let mut pool: Vec<Organism> = Vec::new();
+// Make the pool of organisms. 
+// TODO: Later, this should use the enum to make the pool of organisms.
+pub fn create_organism_pool(size: i32) -> Vec<SimpleOrganism> {
+        // Initialize the empty pool.
+        let mut pool: Vec<SimpleOrganism> = Vec::new();
+        // Create the organisms up to the size. 
         for i in 0..size {
-                let o = Organism::new(i, DNA_SIZE.try_into().unwrap());
+                let mut o = SimpleOrganism::new(i, DNA_SIZE.try_into().unwrap());
+                o.evalute_fitness(TargetList.to_vec());
                 pool.push(o);
         }
         pool
 }
 
-pub fn evalute_fitness(pool: &mut Vec<Organism>, target: Vec<f32>) {
-        // Zero fitness should be the worst, but since we're finding the difference, we'll use the opposite.
-        for c in 0..pool.len() {
-                // Assert that the organism's dna and the target's dna are the same size.
-                assert_eq!(pool[c].dna.len(), target.len());
-
-                let mut fitness = 0.0;
-                for (i, dna) in pool[c].dna.iter().enumerate() {
-                        fitness += difference_squared(*dna, target[i]);
-                }
-
-                pool[c].fitness = fitness;
-        }
-}
-
-fn difference_squared(a: f32, b: f32) -> f32 {
+fn difference_squared(a: f64, b: f64) -> f64 {
         let diff = a - b;
         diff * diff
 }
@@ -66,24 +91,21 @@ mod tests {
         use super::*;
         #[test]
         fn test_evalute_fitness() {
-                let mut pool: Vec<Organism> = Vec::new();
-                let test_org = Organism{id: 0, dna: vec![1.0, 1.0, 1.0], fitness: 0.0};
+                let mut test_org = SimpleOrganism{id: 0, dna: vec![1.0, 1.0, 1.0], fitness: 0.0};
                 
-                pool.push(test_org);
+                let target: Vec<f64> = vec![1.0, 2.0, 3.0];
 
-                let target: Vec<f32> = vec![1.0, 2.0, 3.0];
+                test_org.evalute_fitness(target);
 
-                evalute_fitness(&mut pool, target);
+                println!("Wanted fitness: 5.0, got fitness: {}", test_org.fitness);
 
-                println!("Wanted fitness: 5.0, got fitness: {}", pool[0].fitness);
-
-                assert_eq!(pool[0].fitness, 5.0);
+                assert_eq!(test_org.fitness, 5.0);
 
         }
         #[test]
         fn test_create_organism() {
                 // Ensure the DNA length is correct.
-                let o = Organism::new(0, DNA_SIZE.try_into().unwrap());
+                let o = SimpleOrganism::new(0, DNA_SIZE.try_into().unwrap());
                 assert_eq!(o.dna.len(), DNA_SIZE.try_into().unwrap());
 
                 // Ensure that the bounds are obeyed.
@@ -93,4 +115,5 @@ mod tests {
 
                 println!("{:?}", o);
         }
+
 }
--- src/utils/domain.rs
+++ src/utils/domain.rs
@@ -1,7 +1,8 @@
 use rand::Rng;
 
-pub fn random_float(low: f32, high: f32) -> f32 {
+pub fn random_float(low: f64, high: f64) -> f64 {
         let mut rng = rand::thread_rng();
-        let n: f32 = rng.gen_range(low..=high);
+        let n: f64 = rng.gen_range(low..=high);
         n
 }
+
